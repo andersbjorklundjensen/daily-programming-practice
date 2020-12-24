8d0399a65f0dbbb317f99b809d534c2b78c62f1d
root <andersbjorklund@live.com>
2020-12-24 22:37:39 +0000

    Add date field to transaction class

--- banking-kata/src/Transaction.test.ts
+++ banking-kata/src/Transaction.test.ts
@@ -1,11 +1,17 @@
 import Transaction, { TransactionType } from './Transaction';
 
+function getDateStringForToday() {
+  const date = new Date();
+  return `${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`;
+}
+
 describe('Transaction class tests', () => {
   it('should create a $100 deposit transaction correctly', () => {
     const transaction = new Transaction(100, TransactionType.DEPOSIT);
 
     expect(transaction.amount).toBe(100);
     expect(transaction.type).toBe(TransactionType.DEPOSIT);
+    expect(transaction.date).toBe(getDateStringForToday());
   })
 
   it('should create a $100 withdrawl transaction correctly', () => {
@@ -13,5 +19,6 @@ describe('Transaction class tests', () => {
 
     expect(transaction.amount).toBe(100);
     expect(transaction.type).toBe(TransactionType.WITHDRAWL);
+    expect(transaction.date).toBe(getDateStringForToday());
   })
 });
--- banking-kata/src/Transaction.ts
+++ banking-kata/src/Transaction.ts
@@ -4,10 +4,13 @@ export enum TransactionType {
 }
 
 export default class Transaction {
+  public readonly date: string;
   public readonly amount: number;
   public readonly type: TransactionType;
 
   public constructor($amount: number, $type: TransactionType) {
+    const date = new Date();
+    this.date = `${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`;
     this.amount = $amount;
     this.type = $type;
   }
