29d3277a16728aa5975e78be9c6ad51a74bd60ea
root <andersbjorklund@live.com>
2020-12-24 23:23:28 +0000

    Add frame class

---
+++ bowling-game-kata/src/Frame.test.ts
@@ -0,0 +1,7 @@
+import Frame from "./Frame";
+
+describe('Frame class tests', () => {
+  it('should not be able to roll more than twice for a frame', () => {
+    const frame = new Frame();
+  })
+});
